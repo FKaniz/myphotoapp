4a11fd68fee288b81ebd84fe39b31e3244d5d38a
Kaniz_f <kaniz.kf13@gmail.com>
2020-07-23 13:47:05 -0700

    thirdapi

--- src/main/java/com/example/myphotoapp/repository/UserRepository.java
+++ src/main/java/com/example/myphotoapp/repository/UserRepository.java
@@ -3,14 +3,21 @@ package com.example.myphotoapp.repository;
 import com.example.myphotoapp.model.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class UserRepository {
+
+    List<User> userList = new ArrayList();
     public User getUser(){
         User user = new User("kaniz","BC", 12);
         return user;
     }
 
     public User saveUser(User user) {
+        userList.add(user);
+        return user;
     }
 }
 
